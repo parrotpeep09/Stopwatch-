90833530d78af6805b66cfc409c2b4d418260809
Rachel Oleson <parrotpeep09@gmail.com>
2012-11-04 13:30:17 -0600

    Initial Check in

---
+++ Stopwatch/Stopwatch/mainform.vb
@@ -0,0 +1,24 @@
+﻿Public Class mainform
+    Public mystopwatch As stopwatch
+
+    Private Sub btnStart_Click(sender As System.Object, e As System.EventArgs) Handles btnStart.Click, Timer1.Tick
+
+
+    End Sub
+    Private Sub btnStop_Click(sender As System.Object, e As System.EventArgs) Handles btnStop.Click
+    
+   
+        Timer1.Stop()
+
+    End Sub
+
+    Private Sub mainform_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        Dim mystopwatch As New stopwatch
+        lblShowElapsed.Text = mystopwatch.getcurrentelapsedtimestring()
+
+    End Sub
+End Class
---
+++ Stopwatch/Stopwatch/stopwatch.vb
@@ -0,0 +1,26 @@
+﻿Public Class stopwatch
+    Private _start As Date
+    Private _stop As Date
+    Private _lap As Date
+    Private _display As Date
+    Public Sub New()
+
+    End Sub
+    Public Sub New(StartTime As Date, StopTime As Date, LapTime As Date, Display As Date)
+        _start = StartTime
+        _stop = StopTime
+        _lap = LapTime
+        _display = Display
+    End Sub
+
+    Public Function getcurrentelapsedtimestring() As String
+        Dim t As TimeSpan = DateAndTime.Now - _start
+        ' Dim s As String = String.Format("{0}:{1}:{2}", t.Hours, t.Minutes, t.Seconds)
+        Dim hours As String = t.Hours.ToString().PadLeft(2, "0")
+        Dim minutes As String = t.Minutes.ToString.PadLeft(2, "0")
+        Dim seconds As String = t.Seconds.ToString.PadRight(2, "0")
+        Dim s As String = hours & ":" & minutes & ":" & seconds
+        ' t.ToString()
+        Return s
+    End Function
+End Class
